ed3868f605cf180208522b22989c07ebfb554ec0
Joey Lane <joeylane@users.noreply.github.com>
2014-05-19 21:51:52 -0500

    Initial commit

---
+++ Chrome UA-MOD/Form1.Designer.vb
@@ -0,0 +1,178 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox()
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(12, 25)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(197, 21)
+        Me.ComboBox1.Sorted = True
+        Me.ComboBox1.TabIndex = 0
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(215, 25)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(197, 21)
+        Me.ComboBox2.Sorted = True
+        Me.ComboBox2.TabIndex = 1
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(418, 25)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(197, 21)
+        Me.ComboBox3.TabIndex = 2
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(418, 109)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(197, 25)
+        Me.Button1.TabIndex = 3
+        Me.Button1.Text = "Launch Chrome"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Checked = True
+        Me.CheckBox1.CheckState = System.Windows.Forms.CheckState.Checked
+        Me.CheckBox1.Location = New System.Drawing.Point(336, 114)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(76, 17)
+        Me.CheckBox1.TabIndex = 4
+        Me.CheckBox1.Text = "Incognito?"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(12, 75)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(603, 21)
+        Me.ComboBox4.TabIndex = 5
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 9)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(105, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "User-Agent Category"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(212, 9)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(50, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Spoof As"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(415, 9)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(42, 13)
+        Me.Label3.TabIndex = 8
+        Me.Label3.Text = "Version"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 59)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(90, 13)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "User-Agent String"
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.ForeColor = System.Drawing.Color.DarkCyan
+        Me.Label5.Location = New System.Drawing.Point(9, 115)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(129, 13)
+        Me.Label5.TabIndex = 10
+        Me.Label5.Text = "0 available for this version"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(626, 146)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.ComboBox4)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.ComboBox3)
+        Me.Controls.Add(Me.ComboBox2)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Chrome UA-MOD"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents CheckBox1 As System.Windows.Forms.CheckBox
+    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+
+End Class
---
+++ Chrome UA-MOD/Form1.vb
@@ -0,0 +1,339 @@
+﻿Imports System.Net
+Imports System.Text.RegularExpressions
+
+Public Class Form1
+
+    Private crawlers As String = Nothing
+    Private browsers As String = Nothing
+    Private mobilebrowsers As String = Nothing
+    Private consoles As String = Nothing
+    Private offlinebrowsers As String = Nothing
+    Private emailclients As String = Nothing
+    Private linkcheckers As String = Nothing
+    Private emailcollectors As String = Nothing
+    Private validators As String = Nothing
+    Private feedreaders As String = Nothing
+    Private libraries As String = Nothing
+    Private cloudplatforms As String = Nothing
+    Private others As String = Nothing
+
+    Private selectedClientVersionString As String = Nothing
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Try
+
+            Dim client As New WebClient
+
+            'save the returned data to a string, and also a partially parsed string array
+            Dim rawData As String = client.DownloadString("http://www.useragentstring.com/pages/useragentstring.php")
+            rawData = rawData.Substring(rawData.IndexOf("<table "), rawData.IndexOf("</table>") - rawData.IndexOf("<table ") + 1)
+            Dim data As String() = rawData.Split("<br>")
+
+            'first parse out the categories
+            Dim categoryString As String = Nothing
+            For Each item As String In data
+                'ignore the "ALL" category
+                If item.Contains("unterMenuTitel") And Not item.Contains("ALL") Then
+                    categoryString = categoryString & item & "$$$"
+                End If
+            Next
+            'add our categories to a string array
+            Dim categories As String() = categoryString.Split("$$$")
+            'remove empty lines, and add our categories to ComboBox1
+            For Each item In categories
+                If Not item = Nothing Then
+                    ComboBox1.Items.Add(item.Substring(item.LastIndexOf(">") + 1).Trim)
+                End If
+            Next
+
+            'NOW PARSE OUT THE INDIVIDUAL CLIENTS FOR EACH CATEGORY
+
+            'crawlers
+            For Each item As String In rawData.Substring(rawData.IndexOf("CRAWLERS"), rawData.IndexOf("BROWSERS") - rawData.IndexOf("CRAWLERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    crawlers = crawlers & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'browsers
+            For Each item As String In rawData.Substring(rawData.IndexOf("BROWSERS"), rawData.IndexOf("MOBILE BROWSERS") - rawData.IndexOf("BROWSERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    browsers = browsers & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'mobile browsers
+            For Each item As String In rawData.Substring(rawData.IndexOf("MOBILE BROWSERS"), rawData.IndexOf("CONSOLES") - rawData.IndexOf("MOBILE BROWSERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    mobilebrowsers = mobilebrowsers & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'consoles
+            For Each item As String In rawData.Substring(rawData.IndexOf("CONSOLES"), rawData.IndexOf("OFFLINE BROWSERS") - rawData.IndexOf("CONSOLES")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    consoles = consoles & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'offline browsers
+            For Each item As String In rawData.Substring(rawData.IndexOf("OFFLINE BROWSERS"), rawData.IndexOf("E-MAIL CLIENTS") - rawData.IndexOf("OFFLINE BROWSERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    offlinebrowsers = offlinebrowsers & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'email clients
+            For Each item As String In rawData.Substring(rawData.IndexOf("E-MAIL CLIENTS"), rawData.IndexOf("LINK CHECKERS") - rawData.IndexOf("E-MAIL CLIENTS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    emailclients = emailclients & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'link checkers
+            For Each item As String In rawData.Substring(rawData.IndexOf("LINK CHECKERS"), rawData.IndexOf("E-MAIL COLLECTORS") - rawData.IndexOf("LINK CHECKERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    linkcheckers = linkcheckers & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'email collectors
+            For Each item As String In rawData.Substring(rawData.IndexOf("E-MAIL COLLECTORS"), rawData.IndexOf("VALIDATORS") - rawData.IndexOf("E-MAIL COLLECTORS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    emailcollectors = emailcollectors & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'validators
+            For Each item As String In rawData.Substring(rawData.IndexOf("VALIDATORS"), rawData.IndexOf("FEED READERS") - rawData.IndexOf("VALIDATORS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    validators = validators & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'feed readers
+            For Each item As String In rawData.Substring(rawData.IndexOf("FEED READERS"), rawData.IndexOf("LIBRARIES") - rawData.IndexOf("FEED READERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    feedreaders = feedreaders & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'libraries
+            For Each item As String In rawData.Substring(rawData.IndexOf("LIBRARIES"), rawData.IndexOf("CLOUD PLATFORMS") - rawData.IndexOf("LIBRARIES")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    libraries = libraries & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'cloud platforms
+            For Each item As String In rawData.Substring(rawData.IndexOf("CLOUD PLATFORMS"), rawData.IndexOf("OTHER") - rawData.IndexOf("CLOUD PLATFORMS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    cloudplatforms = cloudplatforms & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+            'others
+            For Each item As String In rawData.Substring(rawData.IndexOf("OTHERS"), rawData.IndexOf("</td></tr>") - rawData.IndexOf("OTHERS")).Split("<br /><br />")
+                If item.Contains("unterMenuName") Then
+                    others = others & item.Substring(item.LastIndexOf(">") + 1).Trim & "$$$"
+                End If
+            Next
+
+        Catch ex As Exception
+
+            'display error and then quit if we encounter a problem loading
+            MessageBox.Show(ex.Message, "Chrome UA-MOD Error")
+            End
+
+        End Try
+
+    End Sub
+
+    Private Sub ComboBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox1.SelectedIndexChanged
+
+        If ComboBox1.SelectedItem = "CRAWLERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In crawlers.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "BROWSERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In browsers.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "MOBILE BROWSERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In mobilebrowsers.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "CONSOLES" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In consoles.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "OFFLINE BROWSERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In offlinebrowsers.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "E-MAIL CLIENTS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In emailclients.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "LINK CHECKERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In linkcheckers.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "E-MAIL COLLECTORS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In emailcollectors.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "VALIDATORS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In validators.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "FEED READERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In feedreaders.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "LIBRARIES" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In libraries.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "CLOUD PLATFORMS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In cloudplatforms.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        If ComboBox1.SelectedItem = "OTHERS" Then
+            ComboBox2.Items.Clear()
+            For Each item As String In others.Split("$$$")
+                If Not item = Nothing Then
+                    ComboBox2.Items.Add(item.Trim)
+                End If
+            Next
+        End If
+
+        ComboBox2.SelectedIndex = 0
+
+    End Sub
+
+    Private Sub ComboBox2_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox2.SelectedIndexChanged
+
+        If Not ComboBox2.SelectedItem = "" Then
+
+            ComboBox3.Items.Clear()
+
+            Dim client As New WebClient
+            Dim rawData As String = client.DownloadString("http://www.useragentstring.com/pages/" & ComboBox2.SelectedItem & "/")
+            selectedClientVersionString = rawData.Substring(rawData.IndexOf("<div id='liste'>"), rawData.LastIndexOf("</ul>") - rawData.IndexOf("<div id='liste'>") + 6)
+            
+            Dim regEx As New Regex("<h4>(?<key>.*?)</h4>")
+
+            For Each m As Match In regEx.Matches(selectedClientVersionString)
+                Dim item As String = m.Groups("key").Value
+                'item = item.Substring(item.IndexOf(">") + 1, item.LastIndexOf("<") - item.IndexOf(">") - 1)
+                ComboBox3.Items.Add(item)
+            Next
+
+            ComboBox3.SelectedIndex = 0
+
+        End If
+
+    End Sub
+
+    Private Sub ComboBox3_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ComboBox3.SelectedIndexChanged
+
+        If Not ComboBox3.SelectedItem = "" Then
+
+            ComboBox4.Items.Clear()
+
+            Dim regex1 As New Regex("<h4>" & ComboBox3.SelectedItem & "(?<key>.*?)</ul>")
+            Dim regEx2 As New Regex("<li>(?<key>.*?)</li>")
+
+
+            For Each m As Match In regEx2.Matches(regex1.Match(selectedClientVersionString).Value)
+                Dim item As String = m.Groups("key").Value
+                item = item.Substring(item.IndexOf(">") + 1, item.LastIndexOf("<") - item.IndexOf(">") - 1)
+                ComboBox4.Items.Add(item)
+            Next
+
+            ComboBox4.SelectedIndex = 0
+
+            Label5.Text = ComboBox4.Items.Count & " available for this version"
+
+        End If
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+
+        Dim launcher As New ProcessStartInfo
+        Dim UA As String = ComboBox4.Text
+        launcher.FileName = "chrome"
+        If CheckBox1.Checked = True Then
+            launcher.Arguments = "--incognito --user-agent=" & Chr(34) & UA & Chr(34)
+        Else
+            launcher.Arguments = "--user-agent=" & Chr(34) & UA & Chr(34)
+        End If
+
+        Process.Start(launcher)
+
+    End Sub
+
+End Class
